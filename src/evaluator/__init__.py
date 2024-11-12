8c5ca222413b2df3a4487c3a69f49a7ce73ff2bb
MatthewCYM <chenyiming0v0@gmail.com>
2024-11-12 19:46:53 +0800

    Add support for mcq evaluation

--- src/evaluator/__init__.py
+++ src/evaluator/__init__.py
@@ -2,10 +2,12 @@ from .open import OpenEvaluator
 from .qa import QAEvaluator
 from .harm import HarmEvaluator
 from .ifeval import IFEvaluator
+from .mcq import MCQEvaluator
 
 evaluator_mapping = {
     'harm': HarmEvaluator,
     'qa': QAEvaluator,
     'open': OpenEvaluator,
     'ifeval': IFEvaluator,
+    'mcq': MCQEvaluator,
 }
---
+++ src/evaluator/mcq.py
@@ -0,0 +1,100 @@
+from .base import Evaluator
+import numpy as np
+import random
+
+class MCQEvaluator(Evaluator):
+    def extract_answer(self, response):
+        response = response.lower()
+        if response.startswith('<1>') or response.startswith('<2>') or response.startswith('<3>'):
+            response = response[3:].strip()
+        for template in [
+            'the answer is [CHOICE]',
+            'the correct answer is [CHOICE]',
+            'would select [CHOICE]',
+            'would choose [CHOICE]',
+            'would select option [CHOICE]',
+            'would choose option [CHOICE]',
+            'is \"[CHOICE]\"',
+            'is \"[CHOICE].',
+            'would be [CHOICE]',
+            'would be option [CHOICE]',
+            'would be ([CHOICE])',
+            'would be option ([CHOICE])',
+            'is [CHOICE],',
+            'is typically [CHOICE],',
+            'is typically [CHOICE].',
+            "i'd say [CHOICE].",
+            "option [CHOICE].",
+            "option [CHOICE]:",
+            "option [CHOICE],",
+            "is [CHOICE]:",
+            "is [CHOICE].",
+            "is [CHOICE],",
+            "is: [CHOICE].",
+            "is ([CHOICE])",
+            ":\n[CHOICE].",
+            ":\n[CHOICE])",
+            ":\n[CHOICE],",
+            ": \n[CHOICE].",
+            ":  \n[CHOICE].",
+            ":\n\n[CHOICE].",
+            ":\n\n[CHOICE])",
+            ":\n\n[CHOICE],",
+            ": \n\n[CHOICE].",
+            "is option [CHOICE],",
+            '([CHOICE]) would be',
+            'is ([CHOICE]).',
+            "is [CHOICE])",
+            "is: [CHOICE])",
+            '(option [CHOICE])',
+            'answer is ([CHOICE])',
+            "select option \"[CHOICE]\"",
+            "is: [CHOICE]",
+            " [CHOICE].",
+            " [CHOICE],",
+            " [CHOICE]:",
+            " [CHOICE])",
+            "\"[CHOICE].",
+            "\"[CHOICE],",
+            "\"[CHOICE]:",
+            "([CHOICE])",
+            "\"[CHOICE]\"",
+
+        ]:
+            for choice in ['a', 'b', 'c', 'd']:
+                if template.replace('[CHOICE]', choice) in response:
+                    return choice.upper()
+        for choice in ['a', 'b', 'c', 'd']:
+            if response == choice:
+                return choice.upper()
+            for punc in ['.', ',', ':', ')']:
+                if response.startswith(choice+punc):
+                    return choice.upper()
+
+        if 'would be a.' in response:
+            return 'A'
+        elif 'would be \"a.' in response:
+            return 'A'
+        elif 'the best option from the given choices would be a scorpion (a)' in response:
+            return 'A'
+        else:
+            # print({response})
+            # print('====')
+            return None
+
+
+    def evaluate(self, data):
+        ground_truth = [item['reference'] for item in data]
+        preds = [self.extract_answer(item['response']) for item in data]
+        cnt = 0
+        for idx in range(len(preds)):
+            if preds[idx] == None:
+                preds[idx] = random.choice(['A', 'B', 'C', 'D'])
+                cnt += 1
+        correct_predictions = sum([1 for pred, gt in zip(preds, ground_truth) if pred == gt])
+        total_predictions = len(ground_truth)
+        accuracy = correct_predictions / total_predictions
+        return {
+            'acc': accuracy * 100, 'fail': 100 * cnt / len(preds)
+        }
+
