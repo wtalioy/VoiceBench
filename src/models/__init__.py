d085285d4e8f7dbd49ab32a475c8eb92b90f1e7d
MatthewCYM <chenyiming0v0@gmail.com>
2024-11-14 17:40:26 +0800

    Add support for moshi

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -5,6 +5,7 @@ from .mini_omni import MiniOmniAssistant
 from .mini_omni2 import MiniOmni2Assistant
 from .gpt4o import GPT4oAssistant
 from .naive2 import Naive2Assistant
+from .moshi import MoshiAssistant
 
 model_cls_mapping = {
     'qwen2': Qwen2Assistant,
@@ -14,4 +15,5 @@ model_cls_mapping = {
     'mini_omni': MiniOmniAssistant,
     'mini_omni2': MiniOmni2Assistant,
     'gpt4o': GPT4oAssistant,
+    'moshi': MoshiAssistant,
 }
---
+++ src/models/moshi.py
@@ -0,0 +1,44 @@
+from .base import VoiceAssistant
+
+import torch, math
+from transformers import MoshiForConditionalGeneration, AutoFeatureExtractor, AutoTokenizer
+import librosa
+
+
+class MoshiAssistant(VoiceAssistant):
+    def __init__(self):
+        self.device = 'cuda'
+        self.dtype = torch.float16
+        self.model = MoshiForConditionalGeneration.from_pretrained("kmhf/hf-moshiko", device_map=self.device, torch_dtype=self.dtype,
+                                                              cache_dir='./cache')
+        self.tokenizer = AutoTokenizer.from_pretrained('kmhf/hf-moshiko')
+        self.feature_extractor = AutoFeatureExtractor.from_pretrained('kmhf/hf-moshiko')
+
+    def generate_audio(
+            self,
+            audio,
+    ):
+        assert audio['sampling_rate'] == 16000
+        audio = audio['array']
+        audio = librosa.resample(audio, orig_sr=16000, target_sr=self.feature_extractor.sampling_rate)
+
+        user_input_values = self.feature_extractor(raw_audio=audio, sampling_rate=self.feature_extractor.sampling_rate,
+                                              return_tensors="pt").to(device=self.device, dtype=self.dtype)
+
+        # prepare moshi input values - we suppose moshi didn't say anything while the user spoke
+        moshi_input_values = torch.zeros_like(user_input_values.input_values)
+
+        ratio = self.model.config.audio_encoder_config.frame_rate / self.model.config.sampling_rate
+
+        # prepare moshi input ids - we suppose moshi didn't say anything while the user spoke
+        num_tokens = math.ceil(moshi_input_values.shape[-1] * ratio)
+        input_ids = torch.ones((1, num_tokens), device=self.device, dtype=torch.int64) * self.tokenizer.encode("<pad>")[0]
+
+        output = self.model.generate(input_ids=input_ids, user_input_values=user_input_values.input_values,
+                                moshi_input_values=moshi_input_values, max_new_tokens=2048, return_audio_waveforms=False)
+
+        text_tokens = output.cpu().numpy()
+
+        response = self.tokenizer.batch_decode(text_tokens, skip_special_tokens=True)[0]
+
+        return response
